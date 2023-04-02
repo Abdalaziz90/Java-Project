02f55f0b700ed133554661a20dc3235ff72ff2b0
Abdalaziz90 <abdalaziz_zinati@outlook.com>
2023-04-02 04:31:33 +0300

    update

---
+++ src/myClassG.java
@@ -0,0 +1,11 @@
+
+public class myClassG {
+
+	public static void main(String[] args) {
+		String myName = "Abdalaziz";
+		int myAge = 33;
+		System.out.println(myName);
+		System.out.println(myAge);
+	}
+
+}
